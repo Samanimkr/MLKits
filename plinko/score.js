16fd0361f048b1b839475796ff00675a634e564c
Samanimkr <Samanimkr@outlook.com>
2019-04-27 20:11:36 +0100

    finding the accuracy of the prediction

--- plinko/score.js
+++ plinko/score.js
@@ -1,6 +1,5 @@
 const outputs = [];
-const predictionPoint = 300;
-const k = 100;
+const k = 5;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
     // Ran every time a balls drops into a bucket
@@ -8,9 +7,22 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-    // Write code here to analyze stuff
-    const bucket = _.chain(outputs)
-        .map(row => [distance(row[0]), row[3]])
+    const testSetSize = 50;
+    const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+
+    let numberCorrect = 0;
+    for (i=0; i<testSet.length; i++) {
+        const bucket = knn(trainingSet, testSet[i][0]);
+        if (bucket === testSet[i][3]) {
+            numberCorrect++
+        }
+    }
+    console.log('Accuracy: ', numberCorrect / testSetSize);
+}
+
+const knn = (dataset, predictionPoint) => (
+    _.chain(dataset)
+        .map(row => [distance(row[0], predictionPoint), row[3]])
         .sortBy(row => row[0])
         .slice(0, k)
         .countBy(row => row[1])
@@ -19,11 +31,16 @@ function runAnalysis() {
         .last()
         .first()
         .parseInt()
-        .value();
+        .value()
+);
 
-    console.log('The ball will most likely fall into bucket ', bucket);
+const splitDataset = (dataset, testCount) => {
+    const shuffled = _.shuffle(dataset);
+
+    const testSet = _.slice(shuffled, 0, testCount);
+    const trainingSet = _.slice(shuffled, testCount);
+
+    return [testSet, trainingSet];
 }
 
-function distance(point) {
-    return Math.abs(point - predictionPoint);
-}
+const distance = (pointA, pointB) => Math.abs(pointA - pointB);
